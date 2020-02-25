3baa99270306ae0eec8320822381345ae8a1655f
kingcada <43425896+kingcada@users.noreply.github.com>
2020-02-25 13:11:49 -0800

    Update PowSpec.m

--- PowSpec.m
+++ PowSpec.m
@@ -21,7 +21,7 @@ end
 opts.ra = 1500/2; %Bead radius, nm
 opts.colors = {[.2039 .5961 .8588] [.1608 .5020 .7255]};
 opts.Fmin = 1;
-opts.Fmax = 20e3;
+opts.Fmax = 10e3;
 opts.lortype = 2;
 opts.wV = 9.1e-10;
 
